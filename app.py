2e20bf1c8598dc83f4cc06dab533df6d9c45efef
debasishnayak12 <137148823+debasishnayak12@users.noreply.github.com>
2024-12-09 17:35:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ import pandas as pd
 import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
 # from sklearn.externals import joblib
 
 
 # load the model from disk
-filename = 'nlp_model.pkl'
-clf = pickle.load(open(filename, 'rb'))
-cv=pickle.load(open('tranform.pkl','rb'))
+# filename = 'nlp_model.pkl'
+# clf = pickle.load(open(filename, 'rb'))
+# cv=pickle.load(open('tranform.pkl','rb'))
 app = Flask(__name__)
 
 @app.route('/')
@@ -18,29 +19,24 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-#	df= pd.read_csv("spam.csv", encoding="latin-1")
-#	df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1, inplace=True)
-#	# Features and Labels
-#	df['label'] = df['class'].map({'ham': 0, 'spam': 1})
-#	X = df['message']
-#	y = df['label']
-#	
-#	# Extract Feature With CountVectorizer
-#	cv = CountVectorizer()
-#	X = cv.fit_transform(X) # Fit the Data
-#    
-#    pickle.dump(cv, open('tranform.pkl', 'wb'))
-#    
-#    
-#	from sklearn.model_selection import train_test_split
-#	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-#	#Naive Bayes Classifier
-#	from sklearn.naive_bayes import MultinomialNB
-#
-#	clf = MultinomialNB()
-#	clf.fit(X_train,y_train)
-#	clf.score(X_test,y_test)
-#    filename = 'nlp_model.pkl'
+	df= pd.read_csv("spam.csv", encoding="latin-1")
+	df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1, inplace=True)
+	# Features and Labels
+	df['label'] = df['class'].map({'ham': 0, 'spam': 1})
+	X = df['message']
+	y = df['label']
+	
+	# Extract Feature With CountVectorizer
+	cv = CountVectorizer()
+	X = cv.fit_transform(X) # Fit the Data
+   
+    # pickle.dump(cv, open('tranform.pkl', 'wb'))
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+	clf = MultinomialNB()
+	clf.fit(X_train,y_train)
+	clf.score(X_test,y_test)
+    # filename = 'nlp_model.pkl'
 #    pickle.dump(clf, open(filename, 'wb'))
     
 	#Alternative Usage of Saved Model
@@ -58,4 +54,4 @@ def predict():
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=False)
